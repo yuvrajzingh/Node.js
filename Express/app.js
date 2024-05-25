2e9a808903566cef7101a967372524fa8854b5b3
Yuvraj Singh <yuvisingh1810@gmail.com>
2024-05-25 13:48:03 +0530

    server with HTTP

---
+++ Express/app.js
@@ -0,0 +1,48 @@
+const http = require('http')
+const {readFileSync} = require('fs')
+
+const PORT = 5000
+
+//get all files
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+
+
+const server = http.createServer((req, res)=>{
+    const url = req.url
+    if(url === '/'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write(homePage)
+        res.end()
+    }
+    else if(url === '/about'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }
+    else if(url === '/styles.css'){
+        res.writeHead(200, {'content-type': 'text/css'})
+        res.write(homeStyles)
+        res.end()
+    }
+    else if(url === '/logo.svg'){
+        res.writeHead(200, {'content-type': 'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+    }
+    else if(url === '/browser-app.js'){
+        res.writeHead(200, {'content-type': 'text/javascript'})
+        res.write(homeLogic)
+        res.end()
+    }
+    else{
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.end('<h1>Page not found</h1>')
+    }
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+}) 
