87c2438b328591b32c50fbe8c1097f299a07db5d
Yuvraj Singh <yuvisingh1810@gmail.com>
2024-05-26 23:53:18 +0530

    Express Basic

--- Express/app.js
+++ Express/app.js
@@ -1,48 +1,27 @@
-const http = require('http')
-const {readFileSync} = require('fs')
+const express = require('express');
+const path = require('path')
+const app = express()
 
 const PORT = 5000
 
-//get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+app.use(express.static('./public'))
 
-
-const server = http.createServer((req, res)=>{
-    const url = req.url
-    if(url === '/'){
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write(homePage)
-        res.end()
-    }
-    else if(url === '/about'){
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<h1>About Page</h1>')
-        res.end()
-    }
-    else if(url === '/styles.css'){
-        res.writeHead(200, {'content-type': 'text/css'})
-        res.write(homeStyles)
-        res.end()
-    }
-    else if(url === '/logo.svg'){
-        res.writeHead(200, {'content-type': 'image/svg+xml'})
-        res.write(homeImage)
-        res.end()
-    }
-    else if(url === '/browser-app.js'){
-        res.writeHead(200, {'content-type': 'text/javascript'})
-        res.write(homeLogic)
-        res.end()
-    }
-    else{
-        res.writeHead(404, {'content-type': 'text/html'})
-        res.end('<h1>Page not found</h1>')
-    }
+app.get("/" , (req, res)=>{
+    console.log('User hit the resource')
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
 
-server.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.get("/about", (req, res)=>{
+    res.status(200).send("About")
+})
+
+
+//To set up our own 404 response
+app.all('*', (req, res)=>{
+    res.status(404).send("No page like that")
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running in Port ${PORT}`)
 }) 
