393d749ef281e7ccbeabb532cccb04baa17cfbe2
Yuvraj Singh <yuvisingh1810@gmail.com>
2024-05-29 16:04:32 +0530

    url params & query

--- Express/app.js
+++ Express/app.js
@@ -1,33 +1,65 @@
-const express = require('express');
-const path = require('path')
+const express = require('express')
 const app = express()
+const {products} = require('./data')
 
-const PORT = 5000
-
-// setup static and middleware
-app.use(express.static('./public'))
-
-//There are two ways to send file or HTML
-// one is to put all of them in static
-// other one is SSR
-// So you probably won't use sendfile
-
-app.get("/" , (req, res)=>{
-    console.log('User hit the resource')
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+app.get('/', (req, res)=>{
+    res.send('<h1>Home Page</h1> <a href="/api/products">Products</a>')
 })
 
-app.get("/about", (req, res)=>{
-    res.status(200).send("About")
+app.get('/api/products', (req, res)=>{
+    
+    const newProducts = products.map((product)=>{
+        const {id, name, image} = products;
+        return {id, name, image}
+    })
+    
+    res.json(products)
+})
+
+app.get('/api/products/:productID', (req, res)=>{
+    
+    // console.log(req);
+    // console.log(req.params);
+    const {productID} = req.params;
+    const singleProduct = products.find((product)=> product.id === Number(productID))
+    
+    if(!singleProduct){
+        return res.status(404).send("Product Does not exist")
+    }
+
+    res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res)=>{
+    console.log(req.params);
+    // res.end(`<h1>review ${reviewID}</h1>`)
 })
 
 
-//To set up our own 404 response
-app.all('*', (req, res)=>{
-    res.status(404).send("No page like that")
+app.get('/api/v1/query', (req, res)=>{
+    // console.log(req.query);
+    const {search, limit } = req.query
+    var sortedProducts = [...products]
+
+    if(search){
+        sortedProducts =  sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+
+    if(limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit)) 
+    }
+
+    if(sortedProducts.length < 1){
+        // res.status(200).end("No products matched your search")
+
+        return res.status(200).json({success: true, data: [ ]})
+    }
+
+    res.status(200).json(sortedProducts) 
 })
 
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running in Port ${PORT}`)
-}) 
+app.listen(5000, ()=>{
+    console.log('Server is listening on port 5000')
+})
